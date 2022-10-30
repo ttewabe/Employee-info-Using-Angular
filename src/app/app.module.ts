c4c07f23f581abb725832197333d04380230d4b3
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-10-30 15:34:17 -0700

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextcomponentComponent } from './textcomponent/textcomponent.component';
+import { ImagecomponentComponent } from './imagecomponent/imagecomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextcomponentComponent,
+    ImagecomponentComponent
   ],
   imports: [
     BrowserModule,
